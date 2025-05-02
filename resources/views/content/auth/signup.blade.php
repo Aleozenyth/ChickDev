ec6544d0ec1b2a144d9b4d60f92112ee9c9a4c57
Aleozenyth <124139385+Aleozenyth@users.noreply.github.com>
2025-05-02 17:18:05 +0700

    Update signup.blade.php

--- resources/views/content/auth/signup.blade.php
+++ resources/views/content/auth/signup.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Matdash Free</title>
+  <title>Chickdev</title>
   <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
   <link rel="stylesheet" href="{{ asset ('css/styles.min.css')}}" />
 </head>
